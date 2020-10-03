d3a3f21b7a57297de4c3a9d3c7efbd83d9cd7b3c
Alec <19513869@curtin.edu.au>
2020-10-03 14:56:57 +0800

    Completed P6 implementation and testing

---
+++ Assignment/P6/README.md
@@ -0,0 +1,29 @@
+# Week 6 - OO Basics and Smalltalk
+
+## Program
+
+FizzBuzz in Smalltalk.
+
+I made mine run from 1 to 100 inclusive as with Fortran and Algol's FizzBuzz.
+
+Run with `./fizzbuzz.st` or `gst fizzbuzz.st`.
+
+## Testing
+
+Used testing script `TestFizzBuzz.bash`.
+
+Test with `./fizzbuzz.st | ./TestFizzBuzz.bash`.
+
+The program passes this test.
+
+## Weekly Question
+
+*How does your implementation of fizz buzz here compare with the
+implementations you wrote in Fortran and Algol in terms of readability and
+writability?*
+
+<!-- TODO -->
+
+## Weekly Reflection
+
+<!-- TODO -->
---
+++ Assignment/P6/TestFizzBuzz.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Testing FizzBuzz from 1 to 100"
+
+for i in {1..100}
+do
+    # Get the current output
+    read test
+
+    # Compute expected
+    if [[ $(($i % 3)) == 0 && $(($i % 5)) == 0 ]]
+    then
+        if [ "$test" != "FizzBuzz" ]
+        then
+            echo "FAIL: Number $i, Expected FizzBuzz, got $test"
+        fi
+    elif [ $((i % 5)) == 0 ]
+    then
+        if [ "$test" != "Buzz" ]
+        then
+            echo "FAIL: Number $i, Expected Buzz, got $test"
+        fi
+    elif [ $((i % 3)) == 0 ]
+    then
+        if [ "$test" != "Fizz" ]
+        then
+            echo "FAIL: Number $i, Expected Fizz, got $test"
+        fi
+    else
+        if [ "$test" != "$i" ] && [ "$test" != "+$i" ]
+        then
+            echo "FAIL: Number $i, Expected $i, got $test"
+        fi
+    fi
+done
+
+echo "Test Completed"
